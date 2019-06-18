ee26ffd7de3530b52d874c7f320d3fb09f8beeed
Carolina Calderon <diana.calderon@epn.edu.ec>
2019-06-18 17:55:05 -0500

    conexion con bd postgres

--- backend-lugares-turisticos/src/app.module.ts
+++ backend-lugares-turisticos/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          password: 'root',
+          database: 'lugares-turisticos',
+          entities: [
+
+          ],
+          synchronize: true,
+      }),
+      TypeOrmModule.forFeature([
+
+
+      ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
